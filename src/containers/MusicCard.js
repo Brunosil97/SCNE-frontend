5b3fa40573dda1ff1bc8df6778e47f4fa72a283c
Bruno Silva <brunosilvinosilva@gmail.com>
2020-04-23 20:15:52 +0100

    able to redirect on button path to song streams

--- src/containers/MusicCard.js
+++ src/containers/MusicCard.js
@@ -12,6 +12,19 @@ class MusicCard extends React.Component {
     checkClickedStatus = () => {
         this.setState({clicked: !this.state.clicked})
     }
+
+    buttonRedirect = (name) => {
+        switch (name) {
+            case "spotify":
+                return window.location.href = this.props.spotify
+                break;
+            case "soundcloud":
+                return window.location.href = this.props.soundcloud
+                break;
+                default:
+                    break
+        }
+    }
     
     render() {
         const BASE_URL = "http://localhost:3000"
@@ -28,8 +41,8 @@ class MusicCard extends React.Component {
                 <button onClick={this.checkClickedStatus}>Streams</button>
                 {this.state.clicked 
                 ? <div>
-                    <button>Spotify</button>
-                    <button>Soundcloud</button>
+                    {spotify ? <button name="spotify" onClick={() => this.buttonRedirect("spotify")}>Spotify</button> : null}
+                    {soundcloud ? <button name="soundcloud" onClick={() => this.buttonRedirect("soundcloud")}>Soundcloud</button> : null}
                 </div>: null
                 }
             </div>
