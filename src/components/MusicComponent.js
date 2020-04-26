2309b98e299c67fcafb2b41daddee409ed4cc656
Bruno Silva <brunosilvinosilva@gmail.com>
2020-04-26 20:12:00 +0100

    moved all songform functionality up to dashboard to allow a song to be instantly added instead of re rendered. Made song form into a component.

--- src/components/MusicComponent.js
+++ src/components/MusicComponent.js
@@ -6,7 +6,8 @@ const MusicComponent = (props) => {
     const song = props.songs.map((song, index) => {
        return <MusicCard key={index} song={song}
        title={song.title} artist={song.artist} image={song.image_url}
-       spotify={song.spotify} soundcloud={song.soundcloud} deleteSong={props.deleteSong}/>
+       spotify={song.spotify} soundcloud={song.soundcloud} 
+       deleteSong={props.deleteSong}/>
     })
 
     return (
--- src/containers/MusicDashboard.js
+++ src/containers/MusicDashboard.js
@@ -3,20 +3,31 @@ import NavBar from '../Navbar/Navbar'
 import MusicComponent from '../components/MusicComponent'
 import API from '../API'
 import SearchBar from '../components/SearchBar'
-import SongForm from '../containers/SongForm'
+import SongForm from '../components/SongForm'
+import { DirectUpload } from 'activestorage';
 
 class MusicDashboard extends React.PureComponent {
     constructor(){
         super()
         this.state = { 
             songs: [],
-            searchFilter: ''
-         }
+            searchFilter: '',
+                title: '',
+                artist: '',
+                image: {},
+                spotify: '',
+                soundcloud: ''
+            
+        }
+    }
+
+    getSongs() {
+        API.getFetch("songs")
+        .then(songs => this.setState({songs: songs}))
     }
     
      componentDidMount() {
-        API.getFetch("songs")
-        .then(songs => this.setState({songs: songs}))
+        this.getSongs()
      }
 
     songsFilteredBySearch = () => {
@@ -34,22 +45,68 @@ class MusicDashboard extends React.PureComponent {
 
     deleteSong = (song) => {
         API.deleteFetch(`/songs/${song.id}`)
-        .then(allsongs => this.setState({songs: allsongs ? allsongs : []}))
+        .then(() => this.getSongs())
+        // .then(allsongs => this.setState({songs: allsongs ? allsongs : []}))
     }
 
+    newSongInState = event => {
+        if (event.target.name === "image") {
+            this.setState({
+                [event.target.name]: event.target.files[0]
+            })
+        } else {
+        this.setState({
+            [event.target.name]: event.target.value
+        })}
+    }
+
+    handleSubmit = (event) => {
+        const BASE_URL = "http://localhost:3000"
+        event.preventDefault()
+        let song = {
+            title: this.state.title,
+            artist: this.state.artist,
+            spotify: this.state.spotify,
+            soundcloud: this.state.soundcloud
+        }
+        API.post(`${BASE_URL}/songs`, song)
+        .then(res => res.json())
+        .then(song => {this.uploadFile(this.state.image, song)})
+    }
+
+    uploadFile = (file, song) => {
+
+        const BASE_URL = "http://localhost:3000"
+        const upload_url = `${BASE_URL}/rails/active_storage/direct_uploads`
+        const upload = new DirectUpload(file, upload_url)
+        return upload.create((error, blob) => {
+            if (error) {
+                console.log(error)
+            } else {
+                fetch(`${BASE_URL}/songs/${song.id}`,{
+                    method: "PUT",
+                    headers: {
+                        'Content-Type' : 'application/json',
+                        'Accept': 'application/json'
+                    },
+                    body: JSON.stringify({image: blob.signed_id})
+                }).then(() => this.getSongs())
+            }
+        })
+    }
 
     render() { 
+        const {title, artist, spotify, soundcloud} = this.state
         return ( 
             <div>
                <NavBar signOut={this.props.signOut}/>
-               {localStorage.token ? <SongForm/> : <SearchBar updateSearchFilter={this.updateSearchFilter} />}
-               <MusicComponent songs={this.songsFilteredBySearch()} deleteSong={this.deleteSong}/>
+               {localStorage.token ? 
+               <SongForm handleSubmit={this.handleSubmit} newSongInState={this.newSongInState} 
+               title={title} artist={artist} spotify={spotify} soundcloud={soundcloud}/> 
+               : <SearchBar updateSearchFilter={this.updateSearchFilter} />}
+               <MusicComponent songs={this.songsFilteredBySearch()} deleteSong={this.deleteSong} />
             </div>
          );
-    }
-
-    componentWillUnmount() {
-        this.setState({songs: []})
     }
 }
  
