7be992d004351107a5c70ae810c52885a4b51716
Bruno Silva <brunosilvinosilva@gmail.com>
2020-05-01 13:03:05 +0100

    styled home navigation and added links for each icon

--- src/App.js
+++ src/App.js
@@ -46,10 +46,30 @@ class App extends React.Component {
     localStorage.removeItem("token")
   }
 
+  homeNavigation = (name) => {
+    console.log(name)
+    switch (name) {
+      case "spotify":
+          window.open("https://open.spotify.com/artist/1SazduFu1DwfCmCTLoFZGL", '_blank')
+          break;
+      case "soundcloud":
+          window.open("https://soundcloud.com/onthescne", '_blank')
+          break;
+      case "youtube":
+          window.open("https://www.youtube.com/channel/UCkI9GfKcCWjTJN4FKwJI6JA", '_blank')
+          break;
+      case "instagram":
+          window.open("https://www.instagram.com/onthescne/?hl=en", '_blank')
+          break;
+          default:
+          break
+  }
+  }
+
   render() {
     return (
       <div className="full-background" >
-        <Route exact path="/" component={HomeComponent} />
+        <Route exact path="/" render={(props) => <HomeComponent homeNavigation={this.homeNavigation} {...props}/>} />
         <Route exact path="/about" component={AboutComponent}/>
         <Route exact path="/admin_login" render={(props) => <AdminLogin handleAuthResponse={this.handleAuthResponse} {...props}/>}/>
         <Route exact path="/music" render={(props) => <MusicDashboard {...props} signOut={this.signOut}/>}/>
--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -8,22 +8,26 @@ import soundcloudIcon from '@iconify/icons-logos/soundcloud';
 import youtubeIcon from '@iconify/icons-logos/youtube';
 import instagramIcon from '@iconify/icons-logos/instagram-icon';
 
-
-
 const HomeComponent = (props) => {
     return (
-        <div>
+        <main>
+             <div>
             <NavBar />
             <img className="camo-logo" src={recordLogo} alt=""/>
-            <div className="contact-links">
-                    <Icon className="Youtube" icon={youtubeIcon} name="youtube" />
-                    <Icon className="spotify" icon={spotifyIcon} name="spotify" /> 
-                    <Icon className="soundcloud" icon={soundcloudIcon} name="soundcloud"/> 
-                    <Icon className="instagram" icon={instagramIcon} name="instagram"/> 
+            <div>
+                <div className="contact-links">
+                    <div className="socials">
+                    <Icon className="Youtube" icon={youtubeIcon} onClick={() => props.homeNavigation("youtube")}/>
+                    <Icon className="spotify" icon={spotifyIcon} onClick={() => props.homeNavigation("spotify")}/> 
+                    <Icon className="soundcloud" icon={soundcloudIcon} onClick={() => props.homeNavigation("soundcloud")}/> 
+                    <Icon className="instagram" icon={instagramIcon} onClick={() => props.homeNavigation("instagram")}/> 
+                    </div>
                 </div>
+            </div>
             <Iframe className="video" width="853" height="480" position="relative" 
             src="https://www.youtube.com/embed/TNIfvaars80" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></Iframe>
         </div>
+        </main>
     )
 }
 
